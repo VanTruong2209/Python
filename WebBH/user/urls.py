6a422cbabd0d7125a77014f912dbf22ec497cc50
hvtruong.2209 <hvtruong.2209@gmail.com>
2022-05-26 20:44:26 +0700

    login

--- WebBH/user/urls.py
+++ WebBH/user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 from . import views
-app_name = 'sanpham'
+app_name = 'user'
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.login, name='login'),
 ]
--- WebBH/user/views.py
+++ WebBH/user/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import User
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.    get('password')
+        try:
+            user = User.objects.get(username = username, password = password)
+            return render(request,'home.html')
+        except:
+            return render(request,'login.html')
+    return render(request,'login.html')
